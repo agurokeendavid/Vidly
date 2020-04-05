f4f402349aae2f2fbfe837ea0b495740f69892c7
Keen David Aguro <agurokeendavid@gmail.com>
2020-04-05 14:59:15 +0800

    Finished Section 2 - 015

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -12,19 +13,25 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Random()
         {
-            var model = new Movie()
+            var movie = new Movie()
             {
                 Name = "Shrek!"
             };
-            return View(model);
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer() {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
             
-        }
-
-
-        [Route(@"movies/released/{year}/{month:regex(\d{4}):range(1, 12)}")]
-        public ActionResult ByReleaseYear(int year, int month)
-        {
-            return Content($"{year}/{month}");
         }
     }
 }
