7e848fe29f7826d49bb19bdac6e46100d69dfc52
Keen David Aguro <agurokeendavid@gmail.com>
2020-04-05 14:46:11 +0800

    Finished Section 2 - 013

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -20,7 +20,9 @@ namespace Vidly.Controllers
             
         }
 
-        public ActionResult ByReleaseDate(int year, int month)
+
+        [Route(@"movies/released/{year}/{month:regex(\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
         {
             return Content($"{year}/{month}");
         }
